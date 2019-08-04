0850787824c5a1bd7b264a002025c81ca1419bd8
Akanksha <anegi@luc.edu>
2019-08-04 12:28:26 -0500

    This is a code attempt and logic to add expression evaluation using Stack.

--- src/simplejavacalculator/Calculator.java
+++ src/simplejavacalculator/Calculator.java
@@ -4,7 +4,17 @@ import static java.lang.Math.log10;
 import static java.lang.Math.pow;
 
 public class Calculator {
+    //TODO use for expression implementation
+    private TokenStack operatorStack;
+    private TokenStack valueStack;
+    private boolean error;
+    public Calculator()
 
+    {
+        operatorStack = new TokenStack();
+        valueStack = new TokenStack();
+        error = false;
+    }
     public enum BiOperatorModes {
 
         normal, add, minus, multiply, divide, xpowerofy
@@ -67,6 +77,10 @@ public class Calculator {
 
         return Double.NaN;
     }
+    //public Double calculatExpression(){
+
+
+    //}
 
     public Double calculateMono(MonoOperatorModes newMode, Double num) {
         if (newMode == MonoOperatorModes.square) {
@@ -127,4 +141,101 @@ public class Calculator {
         throw new Error();
     }
 
-}
+
+    //Methods to perform expression evaluation-START
+
+    //TODO This function is implemented to process input expression by breaking them into Token objects
+    // and then calling process Operator to use stack and evaluate the expression. This function needs to be called when user presses expression button
+    // and inputs an expression to evaluate.
+    public String processInput(String input) {
+        // The tokens from expression
+        String[] parts = input.split(" ");
+        Token[] tokens = new Token[parts.length];
+        for (int n = 0; n < parts.length; n++) {
+            tokens[n] = new Token(parts[n]);
+        }
+
+        // Main loop - process all input tokens
+        for (int n = 0; n < tokens.length; n++) {
+            Token nextToken = tokens[n];
+            if (nextToken.getType() == Token.NUMBER) {
+                valueStack.push(nextToken);
+            } else if (nextToken.getType() == Token.OPERATOR) {
+                if (operatorStack.isEmpty() || nextToken.getPrecedence() > operatorStack.top().getPrecedence()) {
+                    operatorStack.push(nextToken);
+                } else {
+                    while (!operatorStack.isEmpty() && nextToken.getPrecedence() <= operatorStack.top().getPrecedence()) {
+                        Token toProcess = operatorStack.top();
+                        operatorStack.pop();
+                        processOperator(toProcess);
+                    }
+                    operatorStack.push(nextToken);
+                }
+            } else if (nextToken.getType() == Token.LEFT_PARENTHESIS) {
+                operatorStack.push(nextToken);
+            } else if (nextToken.getType() == Token.RIGHT_PARENTHESIS) {
+                while (!operatorStack.isEmpty() && operatorStack.top().getType() == Token.OPERATOR) {
+                    Token toProcess = operatorStack.top();
+                    operatorStack.pop();
+                    processOperator(toProcess);
+                }
+                if (!operatorStack.isEmpty() && operatorStack.top().getType() == Token.LEFT_PARENTHESIS) {
+                    operatorStack.pop();
+                } else {
+                    return "Error: unbalanced parenthesis";
+
+                }
+            }
+
+        }
+        // Empty out the operator stack at the end of the input
+        while (!operatorStack.isEmpty() && operatorStack.top().getType() == Token.OPERATOR) {
+            Token toProcess = operatorStack.top();
+            operatorStack.pop();
+            processOperator(toProcess);
+        }
+        // Print the result if no error has been seen.
+        if(error == false) {
+            Token result = valueStack.top();
+            valueStack.pop();
+            if (!operatorStack.isEmpty() || !valueStack.isEmpty()) {
+                return "Expression error.";
+            } else {
+                return String.valueOf(result.getValue());
+            }
+        }
+        else {
+            return "";
+        }
+
+        }
+    private void processOperator(Token t) {
+
+        Token A = null, B = null;
+        if (valueStack.isEmpty()) {
+            System.out.println("Expression error.");
+            error = true;
+            return;
+        } else {
+            B = valueStack.top();
+            valueStack.pop();
+        }
+        if (valueStack.isEmpty()) {
+            System.out.println("Expression error.");
+            error = true;
+            return;
+        } else {
+            A = valueStack.top();
+            valueStack.pop();
+        }
+        Token R = t.operate(A.getValue(), B.getValue());
+        valueStack.push(R);
+    }
+    //Methods to perform expression evaluation-END
+
+
+
+    }
+
+
+
